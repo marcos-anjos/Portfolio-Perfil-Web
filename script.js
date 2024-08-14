fdc4ee422d91d4a8c774549ff64614028221d02f
Marcos Vinicius <marcos.v.m.anjos@hotmail.com>
2024-08-14 20:43:58 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var typed = new Typed(".typedtext", {
+        strings: ["Desenvolvedor Backend"], // Adicione as strings que deseja
+        typeSpeed: 100,  // Velocidade de digitação
+        backSpeed: 100,  // Velocidade de apagar o texto
+        backDelay: 3000, // Pausa antes de começar a apagar
+        loop: false,      // Faz o loop do texto
+        showCursor: false, // Mostra o cursor de digitação        
+        cursorChar: '|', // Caracter do cursor
+    });
+});
+
+
